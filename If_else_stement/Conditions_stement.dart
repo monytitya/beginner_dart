aea66235631c47e875739e96925670d969e4c47f
monytitya <maotityamony23@mekong.edu.kh>
2026-07-05 12:26:02 +0700

    update

---
+++ If_else_stement/Conditions_stement.dart
@@ -0,0 +1,15 @@
+import "dart:io";
+
+void main (List<String> args){
+  int score =100;
+
+  if (score >= 90) {
+    print("Grade: A");
+  } else if (score >= 80) {
+    print("Grade: B");
+  } else if (score >= 70) {
+    print("Grade: C");
+  } else {
+    print("Grade: F");
+  }
+}
---
+++ If_else_stement/Conditions_week.dart
@@ -0,0 +1,17 @@
+import "dart:io";
+
+void main(List<String> args)
+{
+  String day = "Monyday";
+
+  switch(day)
+  {case "Monday": print("Today is monday"); break;
+  case "Tuesday": print("Today is tuesday"); break;
+  case "Wednesday": print("Today is Wednesday"); break;
+  case "Thurday": print("Today is Thurday"); break;
+
+  default:
+      print("Just another day.");
+      break;}
+
+}
